9c4f7794bd850a57856d63aa72f8e3b05489c0e0
Abhi <avvjit@gmail.com>
2026-08-20 22:20:51 +0530

    cors

--- index.js
+++ index.js
@@ -5,7 +5,13 @@ import authRoutes from "./src/routes/authRoutes.js";
 import cors from "cors";
 
 const app = express();
-app.use(cors())
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://cryptomintx.co.in"
+  ],
+  credentials: true,
+}));
 
 app.use(express.json());
 app.use("/api/auth", authRoutes);
