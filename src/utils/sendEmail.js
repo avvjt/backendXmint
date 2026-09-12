87fc23abc1684ead8f7a9b633828cae797b1043f
Abhi <avvjit@gmail.com>
2026-09-12 16:26:47 +0530

    send email fix

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -6,16 +6,35 @@ const transporter = nodemailer.createTransport({
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+
+  // Don't wait forever if Gmail connection fails
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
 });
 
 const sendEmail = async (to, subject, text, html) => {
-  await transporter.sendMail({
-    from: `"CryptoMintX Security" <${process.env.EMAIL_USER}>`,
-    to,
-    subject,
-    text,
-    html,
-  });
+  try {
+    console.log("EMAIL: Connecting to Gmail...");
+    console.log("EMAIL USER:", process.env.EMAIL_USER);
+
+    const info = await transporter.sendMail({
+      from: `"CryptoMintX Security" <${process.env.EMAIL_USER}>`,
+      to,
+      subject,
+      text,
+      html,
+    });
+
+    console.log("EMAIL: Sent successfully");
+    console.log("EMAIL MESSAGE ID:", info.messageId);
+
+    return info;
+  } catch (error) {
+    console.error("EMAIL SEND ERROR:", error);
+
+    throw error;
+  }
 };
 
 export default sendEmail;
