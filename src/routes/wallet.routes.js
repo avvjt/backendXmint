4edc65f8b64375189ba76dd2b8149b89476030f3
Abhi <avvjit@gmail.com>
2026-09-14 09:38:18 +0530

    backend import fix

--- src/routes/wallet.routes.js
+++ src/routes/wallet.routes.js
@@ -6,7 +6,7 @@ import {
   getTransactions,
 } from "../controllers/wallet.controller.js";
 
-import protect from "../middleware/auth.middleware.js";
+import protect from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
--- src/routes/withdrawal.routes.js
+++ src/routes/withdrawal.routes.js
@@ -5,7 +5,7 @@ import {
   updateWithdrawalStatus,
 } from "../controllers/withdrawal.controller.js";
 
-import protect from "../middleware/auth.middleware.js";
+import protect from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
